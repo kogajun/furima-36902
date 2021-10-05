2f5598ce09274beded241238e14d510a228627b7
kogajun <jannoir890118@yahoo.co.jp>
2021-10-05 17:49:41 +0900

    ユーザー管理機能

--- db/migrate/20211004013300_create_items.rb
+++ db/migrate/20211004013300_create_items.rb
@@ -1,7 +1,7 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      #t.references :user,               null: false, foreign_key: true
+      
       t.string     :item_name,          null: false
       t.text       :explanation,        null: false
       t.integer    :category_id,        null: false
