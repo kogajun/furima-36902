bfd3c1a661e17ffcf4f4efc073c2cb982cec73cf
kogajun <jannoir890118@yahoo.co.jp>
2021-10-17 13:54:41 +0900

    コード修正２

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,12 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :create]
+  before_action :authenticate_user!
 
   def index
     @order_address = OrderAddress.new
     set_item
-    move_to_index
+    if @item.order.present?
+      redirect_to root_path
+    end
   end
 
   def create
